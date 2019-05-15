d8375372296042c3f70551e20b38a1ca6d1c35fc
sergeykulak <kulaksd@gmail.com>
2019-05-15 21:38:58 +0300

    Init commit

---
+++ info-service/src/main/java/com/acme/info/InfoController.java
@@ -0,0 +1,21 @@
+package com.acme.info;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController("/api/info")
+public class InfoController {
+    @Value("${acme.app.version}")
+    private String appVersion;
+
+    @Value("${acme.app.name}")
+    private String appName;
+
+    @GetMapping("welcome")
+    public String welcome() {
+        return String.format("%s %s", appName, appVersion);
+    }
+}

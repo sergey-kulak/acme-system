34a16f7c24455645913cb522bfe8cb22067f8713
Sergey Kulak <siarhei.kulak@itechart-group.com>
2021-10-19 16:52:30 +0300

    Build

--- client-ui/src/common/publicPointNotificationService.js
+++ client-ui/src/common/publicPointNotificationService.js
@@ -8,7 +8,8 @@ let currentConnection
 let subscribers = []
 
 function initClient() {
-    wsClient = new RSocketWebSocketClient({ url: `ws://${window.location.hostname}:7004/pp-service-rsocket` })
+    wsClient = new RSocketWebSocketClient({ url: 
+        `ws://${window.location.hostname}:${window.location.port}/pp-service-rsocket` })
     client = new RSocketClient({
         serializers: JsonSerializers,
         setup: {
